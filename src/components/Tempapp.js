330b13baf17ae293daaf2f6aaf81ce62701bf02e
sakshi910 <sakshivarshney0910@gmail.com>
2021-06-17 09:13:20 +0530

    react_weather_app

---
+++ src/components/Tempapp.js
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from 'react';
+import './css/style.css'
+
+const Tempapp = () => {
+
+
+    const [city, setCity] = useState(null);
+    const [search, setSearch] = useState("Mumbai");
+    useEffect(() => {
+        const fetchApi = async () => {
+            const url = `http://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=f032cfb069f033724ccd6de7f2d16cdc`;
+            const response = await fetch(url);
+            const resJson = await response.json();
+
+            setCity(resJson.main);
+        }
+        fetchApi();
+    }, [search])
+
+    return (
+        <>
+            <div className='box'>
+
+             <div className='inputData'>
+                    <input
+                        type="search"
+                        value={search}
+                        className="inputField"
+                        onChange={(event) => {
+                            setSearch(event.target.value)
+                        }}
+                    ></input>
+             </div>
+                {
+                    !city ? (
+                        <h1 className="errorMsg">No Data Found</h1>
+                    ) : (
+                        <div>
+                            <div className="info">
+                                <h2 className='location'>
+                                    <i className="fas fa-street-view"></i>{search}
+                                </h2>
+                                <h1 className="temp">
+                                    { city.temp }°Cel
+                                </h1>
+                                <h3 className='tempmin_max'>Min : { city.temp_min }°Cel | Max :  { city.temp_max }°Cel </h3>
+
+                            </div>
+
+                            <div className="wave -one"></div>
+                            <div className="wave -two"></div>
+                            <div className="wave -three"></div>
+                        </div>)
+
+                }
+
+
+            </div>
+        </>
+    )
+}
+
+export default Tempapp;
